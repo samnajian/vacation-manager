54c7012c8262715ce5210e3884711ba18d91c3bc
Sam Najian <sam.najian@stylelounge.de>
2018-07-29 13:40:21 +0200

    fix: cleanup

--- dev/js/components/NewEmployee.tsx
+++ dev/js/components/NewEmployee.tsx
@@ -1,48 +1,85 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'react-proptypes';
 import Row from './elements/Row';
 import Button from './elements/Button';
 import Input from './elements/Input';
 import Label from './elements/Label';
+import IEmployee from '../reducers/employee/IEmployee';
 
-const NewEmployee = ({save}) => {
-    let values = {};
+interface INewEmpyloeeFormProps {
+    save: (employee: IEmployee) => {}
+}
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        console.log(values);
-        save(values);
+interface INewEmployeeState {
+    employee: IEmployee;
+}
+
+class NewEmployee extends Component<INewEmpyloeeFormProps, INewEmployeeState> {
+
+    state: INewEmployeeState = {
+        employee: {} as IEmployee
     };
 
-    const onInputChange = e => values = {...values, [e.target.name]: e.target.value};
+    static propTypes = {
+        save: PropTypes.func.isRequired
+    };
 
-    return (
-        <Row>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <Label htmlFor='employeeFirstName'>
-                        First Name
-                    </Label>
-                    <Input type='text' id='employeeFirstName' name='employee[firstName]' onChange={onInputChange}/>
-                </div>
-                <div>
-                    <Label htmlFor='employeeLastName'>
-                        Last Name
-                    </Label>
-                    <Input type='text' id='employeeLastName' name='employee[lastName]' onChange={onInputChange}/>
-                </div>
-                <div>
-                    <Label htmlFor='employeeVacationEntitlement'>
-                        Allowed vacations:
-                    </Label>
-                    <Input type='number' id='employeeVacationEntitlement' name='employee[vacationEntitlement]'
-                           onChange={onInputChange} min='0'/>
-                </div>
-                <div>
-                    <Button type='submit' primary={true}>Save</Button>
-                </div>
-            </form>
-        </Row>
-    );
-};
+    static defaultProps = {};
+
+    private validate = (employee: IEmployee) => {
+        return true;
+    };
+
+    private handleSubmit = e => {
+        e.preventDefault();
+        if (this.validate(this.state.employee)) {
+            this.props.save(this.state.employee);
+        }
+    };
+
+    private onInputChange = e => {
+        const key = e.currentTarget.name;
+        const value = e.target.value;
+        this.setState(state =>
+            ({
+                ...state,
+                employee: {...state.employee, [key]: value}
+            })
+        )
+    };
+
+    render() {
+        return (
+            <Row>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <Label htmlFor='employeeFirstName'>
+                            First Name
+                        </Label>
+                        <Input type='text' id='employeeFirstName' name='employee[firstName]'
+                               onChange={this.onInputChange}/>
+                    </div>
+                    <div>
+                        <Label htmlFor='employeeLastName'>
+                            Last Name
+                        </Label>
+                        <Input type='text' id='employeeLastName' name='employee[lastName]'
+                               onChange={this.onInputChange}/>
+                    </div>
+                    <div>
+                        <Label htmlFor='employeeVacationEntitlement'>
+                            Allowed vacations:
+                        </Label>
+                        <Input type='number' id='employeeVacationEntitlement' name='employee[vacationEntitlement]'
+                               onChange={this.onInputChange} min='0'/>
+                    </div>
+                    <div>
+                        <Button type='submit' primary={true}>Save</Button>
+                    </div>
+                </form>
+            </Row>
+        );
+    }
+}
 
 export default NewEmployee;
--- dev/js/utils/localStorage.ts
+++ dev/js/utils/localStorage.ts
@@ -2,9 +2,12 @@ import IEmployeesState from '../reducers/employee/IEmployeesState';
 import IRequestState from '../reducers/request/IRequestState';
 import IState from '../reducers/state/IState';
 
+//constants
 export const STORE_KEY = '@@VM/State';
 
+//private functions
 const getStoredData = (): IState => JSON.parse((localStorage.getItem(STORE_KEY) || '{}'));
 
-export const getEmployees = (): IEmployeesState => getStoredData()['employees'] || {} as IEmployeesState;
-export const getRequests = (): IRequestState => getStoredData()['requests'] || {} as IRequestState;
+//exported functions
+export const getEmployees = (): IEmployeesState => getStoredData().employees || {employees: []} as IEmployeesState;
+export const getRequests = (): IRequestState => getStoredData().requests || {requests: []} as IRequestState;
